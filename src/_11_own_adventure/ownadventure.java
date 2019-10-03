7ce9d05e698433f864feaf4cdb11bf9dd98c035d
League Student <league@iMac-37.attlocal.net>
2019-10-02 20:00:03 -0700

    g
    g

--- src/_11_own_adventure/ownadventure.java
+++ src/_11_own_adventure/ownadventure.java
@@ -1,5 +1,46 @@
 package _11_own_adventure;
 
-public class ownadventure {
+import javax.swing.JOptionPane;
 
-}
+public class ownadventure {
+public static void main(String[] args) {
+JOptionPane.showMessageDialog(null, "You are doing a choose your own adventure. you will type 1 for option 1 and 2 for option 2");	
+String one=JOptionPane.showInputDialog("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}}
