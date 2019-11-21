d42251cb508ba9e7df8b0991c42ce97f67b4853b
League Student <league@iMac-17.attlocal.net>
2019-11-20 19:59:05 -0800

    r
    r

--- src/_11_own_adventure/ownadventure.java
+++ src/_11_own_adventure/ownadventure.java
@@ -5,7 +5,9 @@ import javax.swing.JOptionPane;
 public class ownadventure {
 public static void main(String[] args) {
 JOptionPane.showMessageDialog(null, "You are doing a choose your own adventure. you will type 1 for option 1 and 2 for option 2");	
-String one=JOptionPane.showInputDialog("");
+String one=JOptionPane.showInputDialog("You walk up to a house with a locked door. Do you break through the door or go around into a window? (1 for door, 2 for window)");
+int two=parseInt(one);
+if(two==1) {String three=JOptionPane.showInputDialog("You go through the door");}
 
 
 
@@ -18,7 +20,6 @@ String one=JOptionPane.showInputDialog("");
 
 
 
-
 	
 	
 	
@@ -43,4 +44,9 @@ String one=JOptionPane.showInputDialog("");
 	
 	
 
+}
+
+private static int parseInt(String one) {
+	// TODO Auto-generated method stub
+	return 0;
 }}
