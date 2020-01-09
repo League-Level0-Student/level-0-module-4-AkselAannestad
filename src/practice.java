9c3fa5409ec0592affff2ae7ccb7f6a03f2e9145
League Student <league@WTS14-3.attlocal.net>
2020-01-08 19:59:54 -0800

    cursed
    with knowlege

---
+++ src/practice.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class practice {
+public static void main(String[] args) {
+	String xa=JOptionPane.showInputDialog("put in a number");
+	int xb=Integer.parseInt(xa);
+	String ya=JOptionPane.showInputDialog("put in a number");
+	int yb=Integer.parseInt(ya);
+	String za=JOptionPane.showInputDialog("put in a number");
+	int zb=Integer.parseInt(za);
+	if(xb>yb) {}
+	
+
+}}
